1f121bf07f6ef0b4871eb490aa2925e059de78ba
Etisalat <Etisalat@ESS17001DXB0226>
2019-12-24 15:44:39 +0400

    - Add new example for test cases

---
+++ TestUtilities/src/test/java/com/java/junit/utils/MathUtilsTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.junit.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Etisalat
+ *
+ */
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("this is run test");
+	}
+
+}
